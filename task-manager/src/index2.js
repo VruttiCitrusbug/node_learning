e46d863b8e65f75dc3de3f29529cbd247ead8be5
Vrutti Patel <vrutti.citrusbug@gmail.com>
2023-08-14 09:49:27 +0530

    14-8-23

--- task-manager/src/index2.js
+++ task-manager/src/index2.js
@@ -2,6 +2,9 @@ const express = require('express')
 require('./db/mongoose-express')
 const User = require('./models/user')
 const userroute = require('./router/user')
+const bcrypt = require('bcryptjs')
+const { request } = require('express')
+
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -35,54 +38,54 @@ app.post('/users',async (req, res) => {
 })
 
 
-//get all objects 200 ok 404 not found
-app.get('/users',async (req,res)=>{
-    try {
-        const user = await User.find({})
-        res.send(user)
-        //[
-        //     {
-        //         "_id": "64d735886d133578ece84d22",
-        //         "name": "vrutti",
-        //         "age": 20,
-        //         "email": "vp@g.co",
-        //         "password": "aa12322a",
-        //         "__v": 0
-        //     },
-        //     {
-        //         "_id": "64d8ee062fbbe9e0fde6dc9e",
-        //         "name": "me",
-        //         "email": "hehe@gm.co",
-        //         "password": "mememem",
-        //         "__v": 0
-        //     }
-        // ]
-    }
-    catch(e){
-        res.status(500).send(e)
-    }
-})
+// //get all objects 200 ok 404 not found
+// app.get('/users',async (req,res)=>{
+//     try {
+//         const user = await User.find({})
+//         res.send(user)
+//         //[
+//         //     {
+//         //         "_id": "64d735886d133578ece84d22",
+//         //         "name": "vrutti",
+//         //         "age": 20,
+//         //         "email": "vp@g.co",
+//         //         "password": "aa12322a",
+//         //         "__v": 0
+//         //     },
+//         //     {
+//         //         "_id": "64d8ee062fbbe9e0fde6dc9e",
+//         //         "name": "me",
+//         //         "email": "hehe@gm.co",
+//         //         "password": "mememem",
+//         //         "__v": 0
+//         //     }
+//         // ]
+//     }
+//     catch(e){
+//         res.status(500).send(e)
+//     }
+// })
 
-//To get specific objects
-app.get('/users/:id',async (req,res)=>{
-    console.log(req.params)//64d494021fe1b11853790111
-    const _id = req.params.id
-    // try {
-    //     const user = await User.find({})
-    //     res.send(user)
-    // }
-    // catch(e){
-    //     res.status(500).send(e)
-    // }
-    try {
-        const user = await User.findById(_id)
-        res.send(user)
-    }
-    catch(e){
-        res.status(400).send(e)
-    }
+// //To get specific objects
+// app.get('/users/:id',async (req,res)=>{
+//     console.log(req.params)//64d494021fe1b11853790111
+//     const _id = req.params.id
+//     // try {
+//     //     const user = await User.find({})
+//     //     res.send(user)
+//     // }
+//     // catch(e){
+//     //     res.status(500).send(e)
+//     // }
+//     try {
+//         const user = await User.findById(_id)
+//         res.send(user)
+//     }
+//     catch(e){
+//         res.status(400).send(e)
+//     }
 
-})
+// })
 
 
 app.listen(port,()=>{
@@ -115,6 +118,7 @@ app.listen(port,()=>{
 
 app.patch('/users/:id',async (req,res)=>{
     const updates = Object.keys(req.body)
+    console.log(updates,"PPPPPPPPPPP")
     const allowupdates = ['name']
     const isvalid = updates.every((update)=>{
         return allowupdates.includes(update)
@@ -127,7 +131,18 @@ app.patch('/users/:id',async (req,res)=>{
         // {
         //     "hright":"P"
         // }
-        const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})//if not exis add new
+        // const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})//if not exis add new
+        const user = await User.findById(req.params.id)
+        user.name = "something else"
+        user.forEach((update)=>{
+            console.log(update)
+            user[update]=request.body[update]
+        })
+        await user.save()
+        if(!user){
+            return res.status(400).send()
+        }
+
         if(!user){
             res.status(400).send()
         }
@@ -143,6 +158,87 @@ app.patch('/users/:id',async (req,res)=>{
 app.delete('/users/:id',async (req,res)=>{
     try {
         const user = await User.findByIdAndDelete(req.params.id)//if not exis add new
+        if(!user){
+            res.status(400).send()
+        }
+        res.send(user)//not effected
+        // {"error":"invalid update "}
+    }
+    catch(e){
+        res.status(500).send(e)
+    }
+})
+
+
+
+const myfun = async () =>{
+    const pass ="red1233"
+    const hash = await bcrypt.hash(pass,8)//8 time execution of one algorithm
+    console.log(hash)//$2a$08$MfGVwhbjOYnciiu2zGBzQ.oYglVVz9Az1yflsL.xZJglHCPU547ju
+    console.log(pass)//red1233
+    const ismatch = await bcrypt.compare(pass,hash)
+    console.log(ismatch)//true
+}
+myfun()
+
+
+
+
+// app.patch('/users/:id',async (req,res)=>{
+//     try {
+//         // {
+//         //     "hright":"P"
+//         // }
+//         const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})//if not exis add new
+//         if(!user){
+//             res.status(400).send()
+//         }
+//         res.send(user)//not effected
+//         // {
+//         //     "_id": "64d735886d133578ece84d22",
+//         //     "name": "vrutti",
+//         //     "age": 20,
+//         //     "email": "vp@g.co",
+//         //     "password": "hehehehehhe",
+//         //     "__v": 0
+//         // }
+//     }
+//     catch(e){
+//         res.status(500).send(e)
+//     }
+// })
+
+app.patch('/users/:id',async (req,res)=>{
+    const updates = Object.keys(req.body)//[ 'name', 'email' ]
+    // {
+    //     "name": "jj",
+    //     "email": "jj@gm.co"
+    // }
+    console.log(updates,"PPPPPPPPPPP")
+    const allowupdates = ['name']
+    const isvalid = updates.every((update)=>{
+        return allowupdates.includes(update)
+    })
+    if(! isvalid){
+        return res.status(400).send({"error":"invalid update "})
+    }
+
+    try {
+        // {
+        //     "hright":"P"
+        // }
+        // const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})//if not exis add new
+        const user = await User.findById(req.params.id)
+        user.name = "something else"
+        user.forEach((update)=>{
+            console.log(update)
+            user[update]=request.body[update]
+        })
+        await user.save()
+        if(!user){
+            return res.status(400).send()
+        }
+
         if(!user){
             res.status(400).send()
         }
